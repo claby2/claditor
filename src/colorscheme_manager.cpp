8833d4cb8cd2e1eb3542030d6f79aea76098d8b7
Edward Wibowo <wibow9770@gmail.com>
2020-12-27 22:46:51 +0800

    refactor: use filesystem

--- src/colorscheme_manager.cpp
+++ src/colorscheme_manager.cpp
@@ -1,7 +1,6 @@
 #include "colorscheme_manager.hpp"
 
-#include <dirent.h>
-
+#include <filesystem>
 #include <fstream>
 #include <sstream>
 
@@ -13,27 +12,20 @@ void ColorschemeManager::fetch_colorschemes() {
     // Fetch colorschemes in runtime colors directory
     std::string home_directory = get_home_directory();
     std::string colors_directory = home_directory + "/.config/claditor/colors/";
-    DIR *dir;
-    dirent entry;
-    dirent *result;
-    if ((dir = opendir(colors_directory.c_str())) != 0) {
-        while (readdir_r(dir, &entry, &result) == 0 && result != 0) {
-            std::string file_name = entry.d_name;
-            if (file_name.substr(file_name.find_last_of(".") + 1) == "clad") {
-                // File has .clad file extension
-                // Parse the file
-                std::string colorscheme_name =
-                    file_name.substr(0, file_name.find_last_of("."));
-                std::ifstream file((colors_directory + file_name).c_str(),
-                                   std::ios::in);
-                std::stringstream file_stream;
-                file_stream << file.rdbuf();
-                file.close();
-                Colorscheme colorscheme(file_stream);
-                add_colorscheme(colorscheme_name, colorscheme);
-            }
+
+    for (const std::filesystem::directory_entry &file :
+         std::filesystem::directory_iterator(colors_directory)) {
+        std::filesystem::path file_path = file.path();
+        if (file_path.extension() == ".clad") {
+            std::string filename =
+                colors_directory + file_path.filename().string();
+            std::ifstream file(filename.c_str(), std::ios::in);
+            std::stringstream file_stream;
+            file_stream << file.rdbuf();
+            file.close();
+            Colorscheme colorscheme(file_stream);
+            add_colorscheme(file_path.stem(), colorscheme);
         }
-        closedir(dir);
     }
 }
 
